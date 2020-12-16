2853240ed50cc7b8f294a75b27862538d5fffa20
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-12-16 15:36:21 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -1,11 +1,9 @@
-"""Update UserBot code
-Syntax: .update"""
-
-import git
-from contextlib import suppress
+import asyncio
 import os
 import sys
-import asyncio
+
+import git
+
 from userbot.utils import admin_cmd
 
 # -- Constants -- #
@@ -15,16 +13,15 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "in this case, Updater is unable to identify the branch to be updated."
     "please check out to an official branch, and re-start the updater."
 )
-OFFICIAL_UPSTREAM_REPO = "https://github.com/CyberBoyAyush/CBA-Userbot"
-BOT_IS_UP_TO_DATE = "CBA Userbot is up-to-date!"
+OFFICIAL_UPSTREAM_REPO = "https://github.com/RDX-GANG/RDX-bot/"
+BOT_IS_UP_TO_DATE = "❂🔱╚»★𝐑𝐃𝐗bot★«╝🔱 is on its latest build!❂"
 NEW_BOT_UP_DATE_FOUND = (
-    "**CBA Userbot Update Found For** {branch_name}\n"
+    "🔱╚»★𝐑𝐃𝐗bot★«╝🔱 Update Found For** {branch_name}\n"
     "\n\n{changelog}\n"
     "Pulling Updates !!"
 )
 NEW_UP_DATE_FOUND = (
-    "**New update found for** {branch_name}\n"
-    "Updating And Restarting..."
+    "**New update found for** {branch_name}\n" "Updating And Restarting..."
 )
 REPO_REMOTE_NAME = "temponame"
 IFFUCI_ACTIVE_BRANCH_NAME = "master"
@@ -35,7 +32,7 @@ RESTARTING_APP = "Re-Starting heroku application"
 # -- Constants End -- #
 
 
-#@command(pattern="^.update", outgoing=True)
+# @command(pattern="^.update", outgoing=True)
 @borg.on(admin_cmd(pattern=r"update"))
 async def updater(message):
     try:
@@ -49,16 +46,15 @@ async def updater(message):
 
     active_branch_name = repo.active_branch.name
     if active_branch_name != IFFUCI_ACTIVE_BRANCH_NAME:
-        await message.edit(IS_SELECTED_DIFFERENT_BRANCH.format(
-            branch_name=active_branch_name
-        ))
+        await message.edit(
+            IS_SELECTED_DIFFERENT_BRANCH.format(branch_name=active_branch_name)
+        )
         return False
 
     try:
         repo.create_remote(REPO_REMOTE_NAME, OFFICIAL_UPSTREAM_REPO)
     except Exception as e:
         print(e)
-        pass
 
     temp_upstream_remote = repo.remote(REPO_REMOTE_NAME)
     temp_upstream_remote.fetch(active_branch_name)
@@ -66,30 +62,26 @@ async def updater(message):
     changelog = generate_change_log(
         repo,
         DIFF_MARKER.format(
-            remote_name=REPO_REMOTE_NAME,
-            branch_name=active_branch_name
-        )
+            remote_name=REPO_REMOTE_NAME, branch_name=active_branch_name
+        ),
     )
 
     if not changelog:
-        await message.edit("**Updating Userbot** \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Status** : `Pulling Updates` \n**Thank You For Using CBA Userbot !**")
+        await message.edit(
+            "*✇Updating✇ 🔱╚»★𝐑𝐃𝐗bot★«╝🔱 \n**✮Version✮** : `1.0` \n**🔱ᴛᴇʟᴇᴛʜᴏɴ🔱** : `6.0.9` \n**⚠️ᴄᴜʀʀᴇɴᴛ ʙʀᴀɴᴄʜ⚠️** : `↼🄼🄰🅂🅃🄴🅁⇀` \n**🛡️𝖘𝖙𝖆𝖙𝖚𝖘🛡️** : `«Pulling Updates»` \n『**Thank You For Using this BHARMAND CHODNE WALA BOT !**』"
+        )
         await asyncio.sleep(5)
- 
+
     message_one = NEW_BOT_UP_DATE_FOUND.format(
-        branch_name=active_branch_name,
-        changelog=changelog
+        branch_name=active_branch_name, changelog=changelog
     )
-    message_two = NEW_UP_DATE_FOUND.format(
-        branch_name=active_branch_name
-    )
+    message_two = NEW_UP_DATE_FOUND.format(branch_name=active_branch_name)
 
     if len(message_one) > 4095:
         with open("change.log", "w+", encoding="utf8") as out_file:
             out_file.write(str(message_one))
         await bot.send_message(
-            message.chat_id,
-            document="change.log",
-            caption=message_two
+            message.chat_id, document="change.log", caption=message_two
         )
         os.remove("change.log")
     else:
@@ -100,6 +92,7 @@ async def updater(message):
 
     if Var.HEROKU_API_KEY is not None:
         import heroku3
+
         heroku = heroku3.from_key(Var.HEROKU_API_KEY)
         heroku_applications = heroku.apps()
         if len(heroku_applications) >= 1:
@@ -109,27 +102,32 @@ async def updater(message):
                     if i.name == Var.HEROKU_APP_NAME:
                         heroku_app = i
                 if heroku_app is None:
-                    await message.edit("Invalid APP Name. Please set the name of your bot in heroku in the var HEROKU_APP_NAME.")
+                    await message.edit(
+                        "Invalid APP Name. Please set the name of your bot in heroku in the var HEROKU_APP_NAME."
+                    )
                     return
                 heroku_git_url = heroku_app.git_url.replace(
-                    "https://",
-                    "https://api:" + Var.HEROKU_API_KEY + "@"
+                    "https://", "https://api:" + Var.HEROKU_API_KEY + "@"
                 )
                 if "heroku" in repo.remotes:
                     remote = repo.remote("heroku")
                     remote.set_url(heroku_git_url)
                 else:
                     remote = repo.create_remote("heroku", heroku_git_url)
-                asyncio.get_event_loop().create_task(deploy_start(bot, message, HEROKU_GIT_REF_SPEC, remote))
+                asyncio.get_event_loop().create_task(
+                    deploy_start(bot, message, HEROKU_GIT_REF_SPEC, remote)
+                )
 
             else:
-                await message.edit("Please create the var HEROKU_APP_NAME as the key and the name of your bot in heroku as your value.")
+                await message.edit(
+                    "Please create the var HEROKU_APP_NAME as the key and the name of your bot in heroku as your value."
+                )
                 return
         else:
             await message.edit(NO_HEROKU_APP_CFGD)
     else:
         await message.edit("No heroku api key found in HEROKU_API_KEY var")
-        
+
 
 def generate_change_log(git_repo, diff_marker):
     out_put_str = ""
@@ -138,9 +136,12 @@ def generate_change_log(git_repo, diff_marker):
         out_put_str += f"×[{repo_change.committed_datetime.strftime(d_form)}]: {repo_change.summary} <{repo_change.author}>\n"
     return out_put_str
 
+
 async def deploy_start(bot, message, refspec, remote):
     await message.edit(RESTARTING_APP)
-    await message.edit("**Updating Userbot** \n**Version** : `2.0` \n**Telethon** : `1.15.0` \n**Branch** : `Master` \n**Status** : `Updating & Restarting` \n__You Can Do__ `.alive` __To Check If I am Alive !__")
+    await message.edit(
+        "**✇Updating✇ 🔱╚»★𝐑𝐃𝐗bot★«╝🔱** \n**✮Version✮** : `1.0` \n**🔱ᴛᴇʟᴇᴛʜᴏɴ🔱** : `6.0.9` \n**⚠️ᴄᴜʀʀᴇɴᴛ ʙʀᴀɴᴄʜ⚠️** : `↼🄼🄰🅂🅃🄴🅁⇀` \n**🛡️𝖘𝖙𝖆𝖙𝖚𝖘🛡️** : ` ♾Updating and Restarting ♾` \n__You Can Do__ `.alive` __To Check If I am Alive !__"
+    )
     remote.push(refspec=refspec)
     await bot.disconnect()
     os.execl(sys.executable, sys.executable, *sys.argv)
