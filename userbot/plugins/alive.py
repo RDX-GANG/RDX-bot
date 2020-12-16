14f5ea6378212bed91d581a74a21e57a2d5a9229
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-12-16 15:17:00 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,34 +1,34 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
-#IMG CREDITS: @WhySooSerious
-import asyncio
-from telethon import events
+# IMG + CODe CREDITS: @Sensei_nex
+# some symbols credits goes to @kraken_the_badass
+
 from uniborg.util import admin_cmd
+
 from userbot import ALIVE_NAME
-from telethon.tl.types import ChannelParticipantsAdmins
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/fc3aef09eb9b82d244f97.jpg"
-pm_caption = "`CBA Userbot IS:` **ONLINE**\n\n"
-pm_caption += "**SYSTEM STATUS**\n"
-pm_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n"
-pm_caption += "`DATABASE STATUS:` **Functional**\n"
-pm_caption += "**Current Branch** : `master`\n"
-pm_caption += "**CBA Userbot OS** : `3.14`\n"
-pm_caption += f"**My Boss** : {DEFAULTUSER} \n"
-pm_caption += "**Made By 😎** : @CyberBoyAyush\n\n"
-pm_caption += "Deploy Your Own : [Repo](https://github.com/CyberBoyAyush/CBA-Userbot)\n"
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Senseibot user"
+PM_IMG = "https://telegra.ph/file/90015e33395f6196486ee.gif"
+pm_caption = "`🔥AAO BHARMANNN CHOD DAALE🔥\n"
+pm_caption += "`                  ⇋【🛡️ 𝕾𝖞𝖘𝖙𝖊𝖒 𝖘𝖙𝖆𝖙𝖚𝖘 🛡️】⇌  `\n\n"
+pm_caption += "`🔱─ᴛᴇʟᴇᴛʜᴏɴ─🔱       : `6.0.9**\n` 💎 Python 💎:` 3.8.5\n"
+pm_caption += "`⚠️─ᴄᴜʀʀᴇɴᴛ ʙʀᴀɴᴄʜ─⚠️`: `↼🄼🄰🅂🅃🄴🅁⇀`\n"
+pm_caption += "`🔱╚»★𝐑𝐃𝐗bot★«╝🔱      : `1.0`\n"
+pm_caption += f"♾ ᴍʏ ʟɪᴇɢᴇ ♾       : {DEFAULTUSER} \n"
+pm_caption += "✵─ᴍʏ ᴅᴇᴠ─✵          :  [✞R.I.P.✞](https://t.me/sensei_nex)\n\n"
+pm_caption += "✮ License ✮         : [🔱╚»★𝐑𝐃𝐗bot★«╝🔱](https://github.com/RDX-GANG/RDX-bot/blob/master/LICENSE)\n"
+
 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def friday(alive):
-    chat = await alive.get_chat()
+    await alive.get_chat()
     """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
     await alive.delete()
 
-    
+
 @borg.on(admin_cmd(pattern=r"Alive", allow_sudo=True))
 async def friday(alive):
-    chat = await alive.get_chat()
+    await alive.get_chat()
     """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PM_IMG,caption=pm_caption)
+    await borg.send_file(alive.chat_id, PM_IMG, caption=pm_caption)
 
     
