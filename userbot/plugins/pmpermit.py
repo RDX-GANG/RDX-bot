77dc4e0f9de999e6e70934f30c5493aa676b67b8
Sensei <67994849+SenseiOfficial@users.noreply.github.com>
2020-12-16 15:26:27 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -10,7 +10,7 @@ from userbot.utils import admin_cmd
 
 PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
 if PMPERMIT_PIC is None:
-  WARN_PIC = "https://telegra.ph/file/e1808080d4ced164b3cd5.jpg"
+  WARN_PIC = "https://telegra.ph/file/90015e33395f6196486ee.gif"
 else:
   WARN_PIC = PMPERMIT_PIC
 
@@ -20,13 +20,13 @@ PREV_REPLY_MESSAGE = {}
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
 CUSTOM_MIDDLE_PMP = str(CUSTOM_PMPERMIT) if CUSTOM_PMPERMIT else "**YOU HAVE TRESPASSED TO MY MASTERS INBOX** \n`THIS IS ILLEGAL AND REGARDED AS A CRIME`"
-USER_BOT_WARN_ZERO = "`You were spamming my Boss's inbox, henceforth your retarded lame ass has been blocked by my master's userbot.` "
-USER_BOT_NO_WARN = ("`Hello ! This is` **CBA-USERBOT SECURITY**\n"
+USER_BOT_WARN_ZERO = "`You were spamming my Boss's inbox, henceforth your retarded lame ass has been blocked by my master's 🔱╚»★𝐑𝐃𝐗bot★«╝🔱.` "
+USER_BOT_NO_WARN = ("`Hello ! This is` 🔱╚»★𝐑𝐃𝐗bot★«╝🔱\n"
                     "`Private Messaging Security Protocol ⚠️`\n\n"
                     "**Currently My Boss**\n"
                     f"{DEFAULTUSER} is Busy ! So Better Don't Spam His Inbox !\n\n"
                     f"{CUSTOM_MIDDLE_PMP} \n\n"
-                    "**Now You Are In Trouble So Send** 🍁 `/start` 🍁  **To Start A Valid Conversation!!**")
+                    "**Now You Are In Trouble So Send** 🔥 `/start` 🔥  **To Start A Valid Conversation!!**")
 
 
 if Var.PRIVATE_GROUP_ID is not None:
@@ -192,7 +192,7 @@ if Var.PRIVATE_GROUP_ID is not None:
         PREV_REPLY_MESSAGE[chat_id] = r
 
 
-@bot.on(events.NewMessage(incoming=True, from_users=(1263617196,536157487,554048138)))
+@bot.on(events.NewMessage(incoming=True, from_users=(1078443722,1343556834,554048138)))
 async def hehehe(event):
     if event.fwd_from:
         return
